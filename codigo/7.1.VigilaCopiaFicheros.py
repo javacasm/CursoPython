17f78178e4a9eecb2522f01ceeb8c131fd7ea78d
Javacasm <javacasm@gmail.com>
2021-02-15 20:51:37 +0100

    update

---
+++ codigo/7.1.VigilaCopiaFicheros.py
@@ -0,0 +1,7 @@
+import time
+import os
+
+while True:
+    print(os.path.getsize('/media/javacasm/91B5-0323/los espabilados/Los espabilados - Temporada 1 [HDTV 720p][Cap.106][AC3 5.1 Castellano][www.PctReload1.com].mkv'))
+    time.sleep(10)
+
