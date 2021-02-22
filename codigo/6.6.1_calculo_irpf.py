9da9d447fc58ac1bbb8165f5580c075c84c34c7f
javacasm <javacasm@gmail.com>
2021-02-22 14:29:54 +0100

    update

--- codigo/6.6.1_calculo_irpf.py
+++ codigo/6.6.1_calculo_irpf.py
@@ -9,7 +9,7 @@ https://www.finect.com/usuario/Josetrecet/articulos/como-funciona-renta-tramos-i
 
 Tramos IRPF 2021  Tipos a aplicar
 
-0 - 12.450€              19%              
+      0 - 12.450€        19%              
 12.450€ - 20.200€        24%
 20.200€ - 35.200€        30%
 35.200€ - 60.000€        37%
@@ -18,12 +18,12 @@ Más de 300.000€          47%
 
 """
 
-tramos = [(12450,  0.19),
+tramos = ((12450,  0.19),
           (20200,  0.24),
           (35200,  0.30),
           (60000,  0.37),
           (300000, 0.45),
-          (10e20,  0.47)] # Ponemos un maximo ficticio
+          (10e20,  0.47)) # Ponemos un maximo ficticio
 
 def calculoIRPF(sueldo):
     impuesto_a_pagar = 0 # Iremos sumando el pago de cada tramo
@@ -37,12 +37,15 @@ def calculoIRPF(sueldo):
         ya_pagado = tope_tramo # actualizamos lo pagado
         if ya_pagado == sueldo:
             break # es nuestro ultimo tramo
-        
     print('----------------------------')
-    print('{:7}            {:8}'.format(  ya_pagado, impuesto_a_pagar))
-    print('Neto {} pagas el {:2.2f}%'.format(sueldo - impuesto_a_pagar, impuesto_a_pagar*100/sueldo))
+    print('{:7}            {:8}'.format(ya_pagado, impuesto_a_pagar))
+
+    return sueldo, sueldo-impuesto_a_pagar, impuesto_a_pagar,impuesto_a_pagar*100/sueldo
+
                                 
     
 while True:
     sueldo = float(input('¿Cual es tu sueldo? '))
-    calculoIRPF(sueldo)
+    sueldoBruto, sueldoNeto,impuestos,tipoEfectivo = calculoIRPF(sueldo)
+    print('Neto {} pagas el {:2.2f}%'.format(sueldoNeto,tipoEfectivo))
+
--- x.Excepciones.md
+++ x.Excepciones.md
@@ -1,2 +1,3 @@
 https://www.w3schools.com/python/python_try_except.asp
 
+https://docs.python.org/3.8/library/exceptions.html
