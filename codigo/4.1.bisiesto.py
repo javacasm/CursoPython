f628178a5eec8bf0a61a8a83f7758c0f99d1ee4d
javacasm <javacasm@gmail.com>
2021-02-25 14:32:27 +0100

    corregido error conversion a entero

--- codigo/4.1.bisiesto.py
+++ codigo/4.1.bisiesto.py
@@ -1,5 +1,5 @@
 # Programa que determina si un año es o no bisiesto
-year = input('Introduzca el anio: ')
+year = int(input('Introduzca el anio: '))
 if ((year%400)==0  or (year % 100) ==0 or (year%4)==0): # la regla para saber si es bisiesto
   print('Es bisiesto!!')
 else:
