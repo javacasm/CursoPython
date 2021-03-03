0979e47dd9e57484b4ce3c696e614f3f90c01fe0
javacasm <javacasm@gmail.com>
2021-03-03 11:11:28 +0100

    update

--- codigo/11.6.memory.py
+++ codigo/11.6.memory.py
@@ -1,21 +1,14 @@
 """
 Recursos
 
-Musica
-
-https://freemusicarchive.org/music/sawsquarenoise/dojokratos/stage-3
-Stage 3 by sawsquarenoise  https://creativecommons.org/licenses/by/4.0/
- 
+Musica - free youtube library
 Sonidos de https://mixkit.co/free-sound-effects/game/
 
-# Otra opción https://inventwithpython.com/pygame/chapter3.html
-# https://stackoverflow.com/questions/59121989/blitting-images-onto-a-tile-that-is-part-of-a-grid-in-pygame
-
+Imagenes de https://stackoverflow.com/questions/59121989/blitting-images-onto-a-tile-that-is-part-of-a-grid-in-pygame
 
 """
 import random, pygame, time, sys
-
-pygame.init()
+v = '0.9'
 
 nFilas = 4
 nColumn = 4
@@ -24,46 +17,53 @@ ANCHO_CARTA = 110
 MARGEN_ENTRE_CARTAS = 10
 ALTO_TEXTO = 100
 
-screen_size = ((ANCHO_CARTA + MARGEN_ENTRE_CARTAS) * nColumn
-            , (ANCHO_CARTA + MARGEN_ENTRE_CARTAS) * nFilas + ALTO_TEXTO)
+screen_size = ((ANCHO_CARTA + MARGEN_ENTRE_CARTAS) * nColumn, 
+             (ANCHO_CARTA + MARGEN_ENTRE_CARTAS) * nFilas + ALTO_TEXTO)
 
 # Colores
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
+# Inicializamos pyGame
+pygame.init()
+
 # Fuentes de letras
 ARIAL_100 = pygame.font.SysFont("Arial", ANCHO_CARTA)
 
+# Inicializamos el sonido
 pygame.mixer.init()
-
 volteoSound = pygame.mixer.Sound('music/Volteo.wav')
 
+pygame.mixer.music.load('./music/Fortunate Note - Silent Partner.ogg') 
+pygame.mixer.music.set_volume(0.5 ) # volumen entre 0 y 1.0
+pygame.mixer.music.play()
+
+# Nombre de las imagenes que usaremos
 imageFiles = ['images/bola.png', 'images/bumeran.png', 'images/coche.png',
               'images/cohete.png', 'images/manzana.png', 'images/pez.png',
               'images/polo.png','images/banana.png']
-images = []
 
+images = [] # guardaremos los pygame.image para usarlos
 for imageFile in imageFiles:
     imagen = pygame.image.load(imageFile)
     images.append(imagen)
 
 imagenOculto = pygame.image.load('images/interrogacion.png')
 
-pygame.display.set_caption('Memory')
+pygame.display.set_caption('Memory ' +  v)
 
 display = pygame.display.set_mode(screen_size)
 
-puntuacion = 0
+puntuacion = 0 # Guardamos las parejas descubiertas
 
 class Carta():
     def __init__(self,x,y,text,idImagen):
         self.x = x
         self.y = y
         self.text = text
-        self.isVisible = False
-        self.myRect = (self.x,self.y,ANCHO_CARTA,ANCHO_CARTA)
         self.idImagen = idImagen
+        self.isVisible = False
         self.isVolteado = False
 
     def getText(self):
@@ -110,6 +110,7 @@ for i in range(0,nColumn):
 bRunning = True
 bHayCambios = True
 cartaVolteada1 = cartaVolteada2 = None
+last_carta = None
 while bRunning:
 
     for event in pygame.event.get():
@@ -117,12 +118,13 @@ while bRunning:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
     
-    if pygame.mouse.get_pressed()[0]:
+    if pygame.mouse.get_pressed()[0]: # hemos pulsado el boton izdo
         (mouse_x,mouse_y) = pygame.mouse.get_pos()
-        # print((mouse_x,mouse_y))
         for carta in  cartas:
-            if carta.isInside(mouse_x,mouse_y):
+            if carta.isInside(mouse_x,mouse_y) and last_carta != carta:
+                last_carta = carta
                 carta.setVolteado(True)
                 if cartaVolteada1 == None: # es la primera volteada
                     cartaVolteada1 = carta
@@ -141,28 +143,17 @@ while bRunning:
             puntos = ARIAL_100.render('¡¡GANÓ!!',True,WHITE)
         else:
             puntos = ARIAL_100.render('Puntos: '+str(puntuacion),True,WHITE)
-        display.blit(puntos, (0, nFilas * (ANCHO_CARTA + MARGEN_ENTRE_CARTAS)))
+        display.blit(puntos, (MARGEN_ENTRE_CARTAS, nFilas * (ANCHO_CARTA + MARGEN_ENTRE_CARTAS) - MARGEN_ENTRE_CARTAS))
         bHayCambios = False
         pygame.display.flip()
-        time.sleep(1.0)
     
-    if puntuacion == nFilas*nColumn//2 :
-        pygame.mixer.music.load('./music/Fortunate Note - Silent Partner.ogg') 
-        pygame.mixer.music.set_volume(0.5 ) # volumen entre 0 y 1.0
-        pygame.mixer.music.play()
-        
-
     if cartaVolteada1 != None and cartaVolteada2 != None: # 2 volteadas
-        if cartaVolteada1.text == cartaVolteada2.text:
-            print('Iguales')
-            # pygame.mixer.Sound('music/OK.wav').play()
+        if cartaVolteada1.text == cartaVolteada2.text: # Iguales
             cartaVolteada2.setVisible(True)
             cartaVolteada1.setVisible(True)
             puntuacion += 1
-        else:
-            print('Distintas')
-            # pygame.mixer.Sound('music/error.wav').play()
-
+        else:  # Distintas
+            time.sleep(1.0) # damos un segundo para verlas ....
         cartaVolteada1.setVolteado(False)
         cartaVolteada2.setVolteado(False)
         cartaVolteada1 = cartaVolteada2 = None
