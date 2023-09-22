94ad8b102e0f5f6c7b5439428c354d68aa331125
javacasm <javacasm@gmail.com>
2023-09-22 11:56:02 +0200

    solución tema 5 v2

--- Publicacion_base/soluciones/Tema 5 - Ejercicio.py
+++ Publicacion_base/soluciones/Tema 5 - Ejercicio.py
@@ -4,14 +4,36 @@ base_par = '# '
 base_impar = ' #'
 linea_par = base_par * 8 
 linea_impar = base_impar * 8 
+
+print('Tablero de ajedrez\n')
+
 tablero = (linea_par + '\n' + linea_impar + '\n' ) * 4
 
 print(tablero)
 
 # Solución 2 - Ajedrez con bucle for
 
+print('\nTablero de ajedrez con bulce for\n')
+
 for i in range(8):
-    if i%2 == 0 : # es par
+    if i % 2 == 0 : # es par
         print(linea_par)
     else:
         print(linea_impar)
+
+# Solución triángulo - usando operador *
+
+print('\nTriángulo\n')
+
+for i in range(10):
+    print('*' * i)
+
+# Solución árbol
+
+N = 10
+
+print('\narbolito\n')
+
+for i in range(1,N+1,2):
+    print(' ' * ((N-i)//2) + '*' * i)
+
