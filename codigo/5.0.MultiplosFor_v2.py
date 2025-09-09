6a3eb3da44d31d20c9d9979d6bc261d37c48d7a8
Jose Antonio Vacas <javacasm@gmail.com>
2025-09-09 22:16:47 +0200

    Ejemplo bucle for v2

---
+++ codigo/5.0.MultiplosFor_v2.py
@@ -0,0 +1,13 @@
+# cálculo de múltiplos de 5 y/o 7 entre 1 y 1000 usando bucle for
+
+for numero in range(1, 1001):
+    bMultiplo5 = numero % 5 == 0  # comprobamos si es múltiplo de 5
+    bMultiplo7 = numero % 7 == 0  # comprobamos si es múltiplo de 7
+    strMensaje = ''
+    if bMultiplo5 or bMultiplo7 :
+        strMensaje = f' {numero} es múltiplo'
+        if bMultiplo5 : 
+            strMensaje += ' de 5'
+        if bMultiplo7 :
+            strMensaje += ' de 7'
+        print(strMensaje)
